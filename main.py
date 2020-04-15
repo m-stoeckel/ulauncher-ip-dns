b9bf3dd5f6df91e1d5a3df38b6df196d384122e0
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2020-04-15 16:20:32 +0200

    Added different provider options

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
+
 logger = logging.getLogger(__name__)
+
 
 class MyIpExtension(Extension):
 
@@ -18,19 +20,40 @@ class MyIpExtension(Extension):
 
 
 class KeywordQueryEventListener(EventListener):
+    _providers = {
+        "opendns": ("@resolver1.opendns.com", "myip.opendns.com", "ANY"),
+        "google": ("@ns1.google.com", "o-o.myaddr.l.google.com", "TXT"),
+        "akamai": ("@ns1-1.akamaitech.net", "whoami.akamai.net", "ANY")
+    }
 
     def on_event(self, event, extension):
+        provider = extension.preferences['provider']
+        server, query, qtype = self._providers[provider]
+        logger.debug('Querying IP address from %s', server)
 
-        ip = subprocess.check_output(["dig", "-4", "@resolver1.opendns.com", "ANY", "myip.opendns.com", "+short"]).decode("utf-8").strip()
-        logger.debug('Got external ip: %s', ip)
+        if event.get_keyword() == "ip6":
+            version = "-6"
+        else:
+            version = "-4"
 
-        items = []
-        items.append(ExtensionResultItem(icon='images/icon.png',
-                                        name='External IP: %s' % ip,
-                                        description='Press \'enter\' to copy to clipboard.',
-                                        on_enter=CopyToClipboardAction(ip)))
+        if provider == "akamai" and version == "-6":
+            ip = "Use a different provider for IP6."
+        else:
+            ip = subprocess.check_output(["dig", server, query, qtype, "+short", version])
+            ip = ip.decode("utf-8").strip()
+            logger.debug('Got external ip: %s', ip)
+
+        items = [
+            ExtensionResultItem(
+                icon='images/icon.png',
+                name='External IP: %s' % ip,
+                description='Press \'enter\' to copy to clipboard.',
+                on_enter=CopyToClipboardAction(ip)
+            )
+        ]
 
         return RenderResultListAction(items)
+
 
 if __name__ == '__main__':
     MyIpExtension().run()
